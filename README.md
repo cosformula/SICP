372b40fc7e9d885ce09b372baa1ae04989f81599
formula <m@cosformula.org>
2020-04-26 13:34:17 +0800

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# SICP Problems
+
+## Setup Racket
+
+```bash
+brew cask install racket
+raco setup  # might be optional
+raco pkg install sicp
+```
+
+## REPL
+
+```bash
+alias scheme='racket -l sicp --repl'
+scheme
+```
+
+## Run
+
+```bash
+racket ./problems/1-1.rkt
+```
---
+++ problems/1-6.rkt
@@ -0,0 +1,56 @@
+#lang sicp
+
+;;; sqrt
+
+(define (abs x)
+  (if (< x 0)
+    (- x)
+    x))
+
+(define (square x)
+  (* x x))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x)
+                x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+;;; sqrt test
+(sqrt 9)
+
+;;; Alyssa's new if
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+;;; new if test
+(new-if (= 2 3) 0 5)
+
+(new-if (= 1 1) 0 5)
+
+(define (sqrt-iter-new guess x)
+  (new-if (good-enough? guess x)
+    guess
+    (sqrt-iter-new (improve guess x)
+                  x)))
+
+(define (sqrt-new x)
+  (sqrt-iter-new 1.0 x))
+
+;;; new sqrt test
+
+(sqrt-new 9)
+;;; infinite loop
